341fa95ac5aee75e085407c9f40ab6a78ac14b38
Artem Gerasimov <qtytestmail@gmail.com>
2024-01-22 16:09:55 +0200

    Selenoid support

--- src/test/java/framework/managers/WebDriverManager.java
+++ src/test/java/framework/managers/WebDriverManager.java
@@ -2,7 +2,14 @@ package framework.managers;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.util.Map;
+
+import static framework.util.Props.BROWSER_TYPE;
 import static framework.util.Props.DRIVER_PATH;
 
 public class WebDriverManager {
@@ -26,8 +33,27 @@ public class WebDriverManager {
     }
 
     private void initDriver() {
-        System.setProperty("webdriver.chrome.driver", PropManager.getPropInstance().getProp(DRIVER_PATH));
-        driver = new ChromeDriver();
+        if(BROWSER_TYPE == "local") {
+            System.setProperty("webdriver.chrome.driver", PropManager.getPropInstance().getProp(DRIVER_PATH));
+            driver = new ChromeDriver();
+        }
+        else if(BROWSER_TYPE == "remote") {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName("chrome");
+            capabilities.setVersion("109.0");
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                    "enableVNC", true,
+                    "enableVideo", false
+            ));
+            try {
+                RemoteWebDriver driver = new RemoteWebDriver(
+                        URI.create("http://selenoid:4444/wd/hub").toURL(),
+                        capabilities);
+            }
+            catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void quitDriver() {
--- src/test/java/framework/util/Props.java
+++ src/test/java/framework/util/Props.java
@@ -3,7 +3,8 @@ package framework.util;
 public class Props {
     public static final String BASE_URL = "base.url";
     public static final String DB_PATH = "db.path";
-
+    public static final String SELEN_URL = "selenoid.url";
+    public static final String BROWSER_TYPE = "browser.type";
     public static final String DRIVER_PATH = "chrome.driver.path";
     public static final String DB_USER = "db.user";
     public static final String DB_PASS = "db.pass";
